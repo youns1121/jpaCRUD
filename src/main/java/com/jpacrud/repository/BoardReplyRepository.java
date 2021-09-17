7b46f72557c50ab1e4e7d5bd33bedc95382a1dd8
younsikMin <minys94@daum.net>
2021-09-17 17:55:56 +0900

    게시글 답글 달기 엔티티 제거

--- src/main/java/com/jpacrud/repository/BoardReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jpacrud.repository;
-
-import com.jpacrud.domain.BoardReply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardReplyRepository extends JpaRepository<BoardReply, Long> {
-}
